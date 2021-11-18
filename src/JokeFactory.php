a99974a82b1dc82f3574b2b015fad8cc4595e651
Marc Siewert <mahesi@users.noreply.github.com>
2021-11-18 16:15:56 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,21 +2,23 @@
 
 namespace Mahesi\ChuckNorrisJokes;
 
-class JokeFactory {
+class JokeFactory
+{
+    protected $jokes = [
+        'Chuck Norris doesn\’t read books. He stares them down until he gets the information he wants.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'If you spell Chuck Norris in Scrabble, you win. Forever.',
+    ];
 
-	protected $jokes = [
-		'Chuck Norris doesn\’t read books. He stares them down until he gets the information he wants.',
-		'Time waits for no man. Unless that man is Chuck Norris.',
-		'If you spell Chuck Norris in Scrabble, you win. Forever.',
-	];
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
 
-	public function __construct( array $jokes = null ) {
-		if ( $jokes ) {
-			$this->jokes = $jokes;
-		}
-	}
-
-	public function getRandomJoke() {
-		return $this->jokes[ array_rand( $this->jokes ) ];
-	}
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
 }
