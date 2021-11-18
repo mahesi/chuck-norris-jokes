d7c15759a42380bd8f1aff263e7144a85ea7afc2
Marc Siewert <marc@siewert.info>
2021-11-18 17:16:01 +0100

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -5,30 +5,33 @@ namespace Mahesi\ChuckNorrisJokes\Tests;
 use Mahesi\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
-class JokeFactoryTest extends TestCase {
-	/** @test */
-	public function it_returns_a_random_joke() {
-		$jokes = new JokeFactory([
-			'This is a joke',
-		]);
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertSame('This is a joke', $joke);
-	}
+        $this->assertSame('This is a joke', $joke);
+    }
 
-	/** @test */
-	public function it_returns_a_predefined_joke() {
-		$chuckNorrisJokes = [
-			'Chuck Norris doesn\’t read books. He stares them down until he gets the information he wants.',
-			'Time waits for no man. Unless that man is Chuck Norris.',
-			'If you spell Chuck Norris in Scrabble, you win. Forever.',
-		];
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris doesn\’t read books. He stares them down until he gets the information he wants.',
+            'Time waits for no man. Unless that man is Chuck Norris.',
+            'If you spell Chuck Norris in Scrabble, you win. Forever.',
+        ];
 
-		$jokes = new JokeFactory();
+        $jokes = new JokeFactory();
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertContains($joke, $chuckNorrisJokes);
-	}
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
 }
